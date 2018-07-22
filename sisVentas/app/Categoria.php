35170d05d18fe9edfdd03ea537829d5c265bee8c
Diohandres Hernandez <diohandres1703@hotmail.com>
2018-07-21 23:02:01 -0430

    Se creo el modelo categoria

--- sisVentas/app/Categoria.php
+++ sisVentas/app/Categoria.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    protected $table='categoria';
+
+    protected $primaryKey='idcategoria';
+
+    public $timestamps=false;
+
+    protected $fillable =[
+    	'nombre',
+    	'descripcion',
+    	'condicion'
+    ];
+
+    protected $guarded =[
+    	
+    ];
 }
